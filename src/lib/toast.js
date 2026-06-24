e388a2a0895c7dac4e47c49ec4172b83b4b76de2
Narayon <narayon231206@gmail.com>
2026-06-24 22:53:16 +0600

    feat: add toast notification helper

---
+++ src/lib/toast.js
@@ -0,0 +1,20 @@
+import { toast } from "react-toastify";
+
+export const showToast = ({ message, type = "info", duration = 4000 }) => {
+  const options = { autoClose: duration, position: "top-right" };
+
+  switch (type) {
+    case "success":
+      toast.success(message, options);
+      break;
+    case "error":
+      toast.error(message, options);
+      break;
+    case "warning":
+      toast.warning(message, options);
+      break;
+    default:
+      toast.info(message, options);
+      break;
+  }
+};
